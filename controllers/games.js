dd228759490f4d301f4a8480dfc10e25a1b0a3c2
MellGriffin <missmelissagriffin@gmail.com>
2024-08-01 18:39:44 -0500

    Got new page to work, working on POST route

--- controllers/games.js
+++ controllers/games.js
@@ -5,6 +5,23 @@ const User = require('../models/user.js');
 
 //router logic below
 
+//GET games index page
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('games/index.ejs', {
+            games: currentUser.games, user: currentUser,
+        });
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+});
+
+//GET /games/new
+router.get("/new", (req, res) => {
+    res.render('games/new.ejs');
+});
 
 
 
